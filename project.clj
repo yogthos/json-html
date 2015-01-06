27b7dad293033d91ab0f0a58aa95335e19363fd2
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-01-06 17:33:38 -0500

    bumped up project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject json-html "0.2.6"
+(defproject json-html "0.2.7"
   :description "Provide JSON and get a DOM node with a human representation of that JSON"
   :url "https://github.com/yogthos/json-html"
   :license {:name "MIT License"
