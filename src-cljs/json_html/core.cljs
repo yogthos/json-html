f08e0dcf8ce68a70068812d9e885a1c26d96c385
Yogthos <dmitri.sotnikov@gmail.com>
2015-07-21 00:13:23 -0400

    fixed cljs typo

--- src-cljs/json_html/core.cljs
+++ src-cljs/json_html/core.cljs
@@ -34,7 +34,7 @@
     [:div.jh-type-object [:span.jh-empty-map]]
     [:table.jh-type-object
      [:tbody
-      (for [[k v] (fn [k1 k2] (compare (str k1) (str k2)))]
+      (for [[k v] (into (sorted-map-by (fn [k1 k2] (compare (str k1) (str k2)))) m)]
         [:tr [:th.jh-key.jh-object-key (render k)]
              [:td.jh-value.jh-object-value (render v)]])]]))
 
